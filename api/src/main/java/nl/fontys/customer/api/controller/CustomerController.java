e130161bd7ef6a428c4e8a9b41402527626b864e
Dennis van der Veeke <mrdienns@dyescape.com>
2020-10-05 20:35:28 +0200

    added simple, blank customer create endpoint

--- api/src/main/java/nl/fontys/customer/api/controller/CustomerController.java
+++ api/src/main/java/nl/fontys/customer/api/controller/CustomerController.java
@@ -1,10 +1,18 @@
 package nl.fontys.customer.api.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/api/v1/customer")
 public class CustomerController {
 
+    @PutMapping(value = "/{customerId}")
+    public ResponseBody createCustomer(@PathVariable String customerId) {
+        // Purely a blank endpoint, returning nothing yet.
+        return null;
+    }
 }
