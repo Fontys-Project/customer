a4e50b9eeeb75196066e0248c5134e7f2f389bce
Dennis van der Veeke <mrdienns@dyescape.com>
2020-10-30 17:43:58 +0100

    now using request and response models in controller

--- api/src/main/java/nl/fontys/customer/api/controller/CustomerController.java
+++ api/src/main/java/nl/fontys/customer/api/controller/CustomerController.java
@@ -1,13 +1,14 @@
 package nl.fontys.customer.api.controller;
 
+import nl.fontys.customer.api.model.api.v1.request.RequestCustomer;
+import nl.fontys.customer.api.model.api.v1.response.ResponseCustomer;
 import nl.fontys.customer.api.security.jwt.exception.AuthorizationException;
 import nl.fontys.customer.api.security.jwt.model.Token;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -17,7 +18,7 @@ import javax.servlet.http.HttpServletRequest;
 public class CustomerController {
 
     @GetMapping(value = "/")
-    public ResponseBody getCustomer(HttpServletRequest request) {
+    public ResponseCustomer getCustomer(HttpServletRequest request) {
         Token token = (Token) request.getSession().getAttribute("session");
         if (!token.getPermissions().contains("CUSTOMER_GET_CUSTOMER")) {
             throw new AuthorizationException("Missing permission CUSTOMER_GET_CUSTOMER");
@@ -27,13 +28,13 @@ public class CustomerController {
     }
 
     @PostMapping(value = "/")
-    public ResponseBody createCustomer() {
+    public ResponseCustomer createCustomer(RequestCustomer requestCustomer) {
         // Purely a blank endpoint, returning nothing yet.
         return null;
     }
 
     @PutMapping(value = "/{customerId}")
-    public ResponseBody updateCustomer(@PathVariable String customerId) {
+    public ResponseCustomer updateCustomer(@PathVariable String customerId, RequestCustomer requestCustomer) {
         // Purely a blank endpoint, returning nothing yet.
         return null;
     }
